9e41778d426abd9fcc81b05617ca54f7e8711d03
Hade21 <abdurrohmanmuhammad123@gmail.com>
2023-06-20 12:10:02 +0800

    feat: add responsive to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import { BsFillSuitHeartFill } from "react-icons/bs";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex items-center justify-center gap-1 bg-gradient-to-b from-white via-blue-200 to-blue-200 pb-4 pt-6 text-oxford-blue">
+    <footer className="flex items-center justify-center gap-1 bg-blue-200 py-4 text-sm text-oxford-blue md:text-base xl:text-lg 2xl:text-2xl">
       Made with{" "}
       <b className="text-red-500">
         <BsFillSuitHeartFill />
