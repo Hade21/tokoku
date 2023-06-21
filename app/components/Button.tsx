5c0121453de7483ded22de0cada48fba29d397bf
Hade21 <abdurrohmanmuhammad123@gmail.com>
2023-06-21 14:56:41 +0800

    feat: create product cart

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -8,11 +8,11 @@ interface ButtonProps {
 }
 
 const primary =
-  "bg-royal-blue text-white rounded-md px-4 py-2 hover:bg-bice-blue transition-all duration-100 active:translate-y-1";
+  " w-fit h-fit bg-royal-blue text-sm text-white rounded-md px-4 py-2 hover:bg-bice-blue transition-all duration-100 active:translate-y-1";
 const secondary =
-  "bg-white text-oxford-blue rounded-md px-4 py-2 hover:text-white hover:bg-royal-blue transition-all duration-100 active:translate-y-1";
+  " w-fit h-fit bg-white text-sm ring-2 ring-royal-blue text-oxford-blue rounded-md px-4 py-2 hover:text-white hover:bg-royal-blue transition-all duration-100 active:translate-y-1";
 const tertiary =
-  "bg-transparent text-oxford-blue rounded-md px-4 py-2 hover:text-white hover:bg-royal-blue transition-all duration-100 active:translate-y-1";
+  "w-fit h-fit bg-red-500 text-sm text-white rounded-md px-4 py-2 hover:text-white hover:bg-royal-blue transition-all duration-100 active:translate-y-1";
 
 const Button: React.FC<ButtonProps> = ({
   type,
--- app/components/index.ts
+++ app/components/index.ts
@@ -3,3 +3,5 @@ export { default as Header } from "./Header"
 export { default as HamburgerMenu } from "./HamburgerMenu"
 export { default as HeroBanner } from "./HeroBanner"
 export { default as Button } from './Button'
+export { default as Footer } from './Footer'
+export { default as Products } from "./Products"
