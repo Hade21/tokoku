0c490aa730c7bcddbbd0a89aa8c2877b4fe942b4
Hade21 <abdurrohmanmuhammad123@gmail.com>
2023-06-30 22:44:51 +0800

    feat: use Link to route to login

--- app/user/register/page.tsx
+++ app/user/register/page.tsx
@@ -1,4 +1,5 @@
 import { FormUser, UserAnimation } from "@/app/components";
+import Link from "next/link";
 import React from "react";
 
 const Register: React.FC = () => {
@@ -16,12 +17,11 @@ const Register: React.FC = () => {
       <div className="mt-8">
         <FormUser variant="register" />
         <div className="mt-6 flex items-center justify-center">
-          <a
-            href="#"
-            className="inline-flex items-center text-center text-xs font-thin text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white"
-          >
-            <span className="ml-2">Have an account? Sign in now</span>
-          </a>
+          <Link href="/user/login">
+            <div className="inline-flex items-center text-center text-xs font-thin text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white">
+              <span className="ml-2">Have an account? Sign in now</span>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
