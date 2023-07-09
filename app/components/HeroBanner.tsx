1b921084f1b7a678f48cda8ba9301fc90d4623ec
Hade21 <abdurrohmanmuhammad123@gmail.com>
2023-07-10 05:59:15 +0800

    feat: add contrast color

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Link from "next/link";
 import Button from "./Button";
+import Image from "next/image";
 
 const HeroBanner: React.FC = () => {
   return (
     <div className="mx-auto my-5 flex w-[95vw] flex-col rounded-lg bg-non-photo-blue p-5 dark:bg-penn-blue">
       <p className="text-base">July Flash Sale</p>
       <h3 className="text-4xl font-extrabold">Hayuri Couple</h3>
-      <img
-        src=""
+      <Image
+        src="https://fashionvhal.myshopify.com/products/baju-muslim-couple-gamis-ibu-dan-anak-jersey-mint-peach"
         alt="hero-image"
         className="max-h-24 self-end overflow-visible"
+        width={300}
+        height={300}
       />
       <div className="flex justify-between">
         <Link href="">
