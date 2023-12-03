89fe58b40d190a57cef1e5accd4ce4cb9d32b5c4
Hade21 <abdurrohmanmuhammad123@gmail.com>
2023-12-04 07:17:53 +0800

    feat: styling mobile view

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -4,7 +4,7 @@ import ProfileData from "./profileData";
 
 const Profile: React.FC = () => {
   return (
-    <div className="h-screen w-[calc(100vw-1.5rem)] self-start px-6 py-8">
+    <div className="self-start py-8 sm:w-[calc(100vw-1.5rem)] sm:px-6">
       <div className="mb-6 self-start justify-self-start">
         <BackButton />
       </div>
--- app/user/[id]/profileData.tsx
+++ app/user/[id]/profileData.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useMemo } from "react";
 import { Button } from "@/app/components";
 import Image from "next/image";
 import { FiEdit3 } from "react-icons/fi";
@@ -11,6 +11,7 @@ import {
   changeFirstName,
   changeLastName,
   changePhone,
+  setData,
 } from "@/store/userDataSlice";
 
 const ProfileData: React.FC = () => {
@@ -18,11 +19,24 @@ const ProfileData: React.FC = () => {
   const dispatch = useAppDispatch();
   const profile = useAppSelector((state) => state.userData);
   const { data } = useGetUserData();
-  console.log("🚀 ~ file: profileData.tsx:14 ~ data:", data);
+  console.log("🚀 ~ file: profileData.tsx:14 ~ data:", data?.data.user);
+  useMemo(() => {
+    dispatch(setData(data?.data.user));
+  }, [data]);
   return (
     <div>
-      <section className="mb-4 flex items-center justify-between gap-6 px-6">
-        <nav>
+      <section
+        className={`mb-4 gap-6 transition-all duration-300 sm:px-6 ${
+          edit
+            ? "flex flex-col sm:grid sm:grid-cols-3"
+            : "flex flex-col items-center justify-between sm:flex-row"
+        }`}
+      >
+        <nav
+          className={`${
+            edit ? "row-span-2 w-fit self-center justify-self-center" : ""
+          }`}
+        >
           <div className="flex aspect-square items-center justify-center overflow-hidden rounded-full bg-white p-4">
             <Image
               src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg"
@@ -33,14 +47,18 @@ const ProfileData: React.FC = () => {
             ></Image>
           </div>
         </nav>
-        <div className="flex-1">
+        <div
+          className={`${
+            edit ? "col-span-2 col-start-2 row-start-2" : "flex-grow"
+          }`}
+        >
           <h1 className="text-3xl font-semibold text-oxford-blue">
             {!edit ? (
               `${profile.firstName} ${profile.lastName}`
             ) : (
               <div className="flex w-full flex-col gap-2">
                 <input
-                  className="rounded-sm px-3 py-2 focus:outline-none focus:ring-2 focus:ring-offset-bice-blue"
+                  className="rounded-md px-3 py-2 text-xl outline-none transition-all duration-300 focus:ring-2 focus:ring-offset-bice-blue"
                   type="text"
                   name="firstName"
                   id="firstName"
@@ -49,7 +67,7 @@ const ProfileData: React.FC = () => {
                   onChange={(e) => dispatch(changeFirstName(e.target.value))}
                 />
                 <input
-                  className="rounded-sm px-3 py-2 focus:outline-none focus:ring-2 focus:ring-offset-bice-blue"
+                  className="rounded-md px-3 py-2 text-xl outline-none transition-all duration-300 focus:ring-2 focus:ring-offset-bice-blue"
                   type="text"
                   name="lastName"
                   id="lastName"
@@ -61,7 +79,7 @@ const ProfileData: React.FC = () => {
             )}
           </h1>
         </div>
-        <div className="">
+        <div className="col-span-2 self-end justify-self-end">
           {edit ? (
             <Button
               type="button"
@@ -85,37 +103,39 @@ const ProfileData: React.FC = () => {
           )}
         </div>
       </section>
-      <section className="mb-4 border-t-2 border-white py-2">
-        <h1 className="text-lg font-semibold">Email</h1>
-        {!edit ? (
-          <p className="p-2 text-gray-500">{profile.email}</p>
-        ) : (
-          <input
-            className="rounded-sm px-3 py-2 focus:outline-none focus:ring-2 focus:ring-offset-bice-blue"
-            type="text"
-            name="email"
-            id="email"
-            value={profile.email}
-            placeholder="Your email"
-            onChange={(e) => dispatch(changeEmail(e.target.value))}
-          />
-        )}
-      </section>
-      <section className="mb-4 border-t-2 border-white py-2">
-        <h1 className="text-lg font-semibold">Phone</h1>
-        {!edit ? (
-          <p className="text-gray-500">{profile.phone}</p>
-        ) : (
-          <input
-            className="rounded-sm px-3 py-2 focus:outline-none focus:ring-2 focus:ring-offset-bice-blue"
-            type="text"
-            name="phone"
-            id="phone"
-            value={profile.phone}
-            placeholder="Your phone number"
-            onChange={(e) => dispatch(changePhone(e.target.value))}
-          />
-        )}
+      <section className="mb-4 flex flex-col justify-between border-t-2 border-white py-2 sm:flex-row">
+        <div className="flex-1">
+          <h1 className="text-lg font-semibold">Email</h1>
+          {!edit ? (
+            <p className="p-2 text-gray-500">{profile.email}</p>
+          ) : (
+            <input
+              className="rounded-md px-3 py-2 outline-none transition-all duration-300 focus:ring-2 focus:ring-offset-bice-blue"
+              type="text"
+              name="email"
+              id="email"
+              value={profile.email}
+              placeholder="Your email"
+              onChange={(e) => dispatch(changeEmail(e.target.value))}
+            />
+          )}
+        </div>
+        <div className="flex-1">
+          <h1 className="text-lg font-semibold">Phone</h1>
+          {!edit ? (
+            <p className="text-gray-500">{profile.phone}</p>
+          ) : (
+            <input
+              className="rounded-md px-3 py-2 outline-none transition-all duration-300 focus:ring-2 focus:ring-offset-bice-blue"
+              type="text"
+              name="phone"
+              id="phone"
+              value={profile.phone}
+              placeholder="Your phone number"
+              onChange={(e) => dispatch(changePhone(e.target.value))}
+            />
+          )}
+        </div>
       </section>
       <section className="mb-4 border-t-2 border-white py-2">
         <h1 className="text-lg font-semibold">Addresses</h1>
